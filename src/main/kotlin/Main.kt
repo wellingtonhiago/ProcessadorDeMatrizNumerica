1ba5ec36a2581cd185ee457ee93aabb5c7bb2fbe
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-09-20 16:17:57 -0300

    Estágio 1/6: Adição

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val (rows1, cols1) = readNums()
+    val matrix1 = List(rows1) { readNums() }
+
+    val (rows2, cols2) = readNums()
+    val matrix2 = List(rows2) { readNums() }
+
+    if (rows1 != rows2 || cols1 != cols2) { print("ERROR"); return }
+
+    for (y in 0 until rows1) {
+        for (x in 0 until cols1) {
+            print("${matrix1[y][x] + matrix2[y][x]} ")
+        }
+        println()
+    }
+}
+
+fun readNums() = readln().split(" ").map { it.toInt() }
