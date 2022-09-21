729783111827ad2124da02e559be6808dfbad4aa
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-09-20 22:50:41 -0300

    Estágio 5/6 Determinante

--- src/main/kotlin/StageFour.kt
+++ src/main/kotlin/StageFour.kt
@@ -7,10 +7,28 @@ class Matrix(private val rows: Int, private val cols: Int, fromCli: Boolean = fa
 
     operator fun get(row: Int, col: Int) = matrix[row][col]
     operator fun set(row: Int, col: Int, value: Double) = matrix[row].set(col, value)
+    override fun toString() = matrix.joinToString("\n") { it.joinToString(" ") }
 
-    operator fun plus(other: Matrix) = transform { x, y -> this[y, x] + other[y, x] }
+    private fun transform(action: (x: Int, y: Int) -> Double): Matrix {
+        val newMatrix = Matrix(rows, cols)
+        for (row in 0 until rows)
+            for (col in 0 until cols)
+                newMatrix[row, col] = action(col, row)
+
+        return newMatrix
+    }
+
+    fun transposeMain() = transform { x, y -> this[x, y] }
+    fun transposeSide() = transform { x, y -> this[cols - 1 - x, rows - 1 - y] }
+    fun transposeVertical() = transform { x, y -> this[y, cols - 1 - x] }
+    fun transposeHorizontal() = transform { x, y -> this[rows - 1 - y, x] }
     operator fun times(multiplier: Int) = transform { x, y -> multiplier * this[y, x] }
+    operator fun plus(other: Matrix): Matrix {
+        if (rows != other.rows || cols != other.cols) error("The operation cannot be performed.")
+        return transform { x, y -> this[y, x] + other[y, x] }
+    }
     operator fun times(other: Matrix): Matrix {
+        if (cols != other.rows) error("The operation cannot be performed.")
         val newMatrix = Matrix(rows, other.cols)
         for (row in 0 until rows)
             for (col in 0 until other.cols)
@@ -19,53 +37,28 @@ class Matrix(private val rows: Int, private val cols: Int, fromCli: Boolean = fa
         return newMatrix
     }
 
-    fun transposeMain() = transform { x, y -> this[x, y] }
-    fun transposeSide() = transform { x, y -> this[cols - 1 - x, rows - 1 - y] }
-    fun transposeVertical() = transform { x, y -> this[y, cols - 1 - x] }
-    fun transposeHorizontal() = transform { x, y -> this[rows - 1 - y, x] }
+    fun getDeterminant(m: List<MutableList<Double>> = matrix): Double {
+        if (m.size == 2) return m[0][0] * m[1][1] - m[0][1] * m[1][0]
 
-    private fun transform(operate: (x: Int, y: Int) -> Double): Matrix {
-        val newMatrix = Matrix(rows, cols)
-        for (row in 0 until rows)
-            for (col in 0 until cols)
-                newMatrix[row, col] = operate(col, row)
-
-        return newMatrix
+        var result = 0.0
+        for (col in m.indices) {
+            val subMatrix = m.map { it.toMutableList() }.toMutableList()
+                .apply { removeAt(0); forEach { it.removeAt(col) } }
+            result += (if (col % 2 == 0) 1 else -1) * m[0][col] * getDeterminant(subMatrix)
+        }
+        return result
     }
-
-    fun isEqualSize(other: Matrix) = rows == other.rows && cols == other.cols
-    fun canCompose(other: Matrix) = cols == other.rows
-
-    override fun toString() = matrix.joinToString("\n") { it.joinToString(" ") }
 }
 
 class Processor {
-    private fun printResult(matrix: Matrix) = println("The result is:\n$matrix")
-
-    private fun readTwoMatrices() = readMatrix("first ") to readMatrix("second ")
-    private fun readMatrix(prefix: String = ""): Matrix {
-        print("Enter size of ${prefix}matrix:")
+    private fun printResult(result: Any) = println("The result is:\n$result")
+    val prefix = listOf("", "first ", "second ")
+    private fun readMatrix(num: Int = 0): Matrix {
+        print("Enter size of ${prefix[num]}matrix:")
         val (rows, cols) = readln().split(" ").map { it.toInt() }
-        println("Enter ${prefix}matrix:")
+        println("Enter ${prefix[num]}matrix:")
         return Matrix(rows, cols, true)
     }
-    private fun addMatrices() {
-        val (matrix1, matrix2) = readTwoMatrices()
-        if (!matrix1.isEqualSize(matrix2)) error("The operation cannot be performed.")
-        printResult(matrix1 + matrix2)
-    }
-    private fun scaleMatrix() {
-        val matrix = readMatrix()
-        print("Enter constant:")
-        val multiplier = readln().toInt()
-        printResult(matrix * multiplier)
-    }
-    private fun composeMatrices() {
-        val (matrix1, matrix2) = readTwoMatrices()
-        if (!matrix1.canCompose(matrix2)) error("The operation cannot be performed.")
-        printResult(matrix1 * matrix2)
-    }
-
     private fun transposeMatrix() {
         print(
             """ 
@@ -77,24 +70,24 @@ class Processor {
             """.trimMargin()
         )
         val choice = readln().toInt()
-
         val matrix = readMatrix()
-
-        printResult(when (choice) {
-            1 -> matrix.transposeMain()
-            2 -> matrix.transposeSide()
-            3 -> matrix.transposeVertical()
-            4 -> matrix.transposeHorizontal()
-            else -> error("Not a valid choice!")
-        })
+        printResult(
+            when (choice) {
+                1 -> matrix.transposeMain()
+                2 -> matrix.transposeSide()
+                3 -> matrix.transposeVertical()
+                4 -> matrix.transposeHorizontal()
+                else -> error("Not a valid choice!")
+            }
+        )
     }
-
     private fun showMenu() = print(
         """ 
         |1. Add matrices
         |2. Multiply matrix by a constant
         |3. Multiply matrices
         |4. Transpose matrix
+        |5. Calculate a determinant
         |0. Exit
         |Your choice:
         """.trimMargin()
@@ -103,10 +96,11 @@ class Processor {
         while (true) {
             showMenu()
             when (readln().toInt()) {
-                1 -> addMatrices()
-                2 -> scaleMatrix()
-                3 -> composeMatrices()
+                1 -> printResult(readMatrix(1) + readMatrix(2))
+                2 -> printResult(readMatrix().also { print("Enter constant:") } * readln().toInt())
+                3 -> printResult(readMatrix(1) * readMatrix(2))
                 4 -> transposeMatrix()
+                5 -> printResult(readMatrix().getDeterminant())
                 else -> return
             }
             println()
